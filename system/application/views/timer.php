29f45f0fb12b3736f176781f653ab07694329188
Kyle Cronin <kyle.cronin@gmail.com>
2008-11-29 02:25:27 -0500

    another small change

--- system/application/views/timer.php
+++ system/application/views/timer.php
@@ -1,3 +1,3 @@
 </table>
 <br><small><i>scrape: <?=$pageload?>s&nbsp;&nbsp;&nbsp;process: <?=$dbprocess?>s</i></small>
-<br><small>I have switched over to SQLite, so please <a href="http://stackoverflow.com/questions/61553/track-your-reputation">let me know</a> about any performance issues</small>
+<br><small>I have switched over to SQLite, so please <a href="http://stackoverflow.com/questions/61553/track-your-reputation">let me know</a> if you experience any performance issues</small>
