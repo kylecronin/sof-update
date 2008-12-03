91eacb7b02a9669ca78d85877c75c82fe3637003
Kyle Cronin <kyle.cronin@gmail.com>
2008-12-03 17:42:46 -0500

    added interval as well (much easier to see elapsed time)

--- system/application/views/timer.php
+++ system/application/views/timer.php
@@ -1,7 +1,44 @@
 </table>
 <br><small><i>scrape: <?=$pageload?>s&nbsp;&nbsp;&nbsp;process: <?=$dbprocess?>s</i></small>
 <br><br>
+<?php
+	function RelativeTime($time, $now = false)
+	{
+		$time = (int) $time;
+		$curr = $now ? $now : time();
+		$shift = $curr - $time;
+		
+		$ret = "";
+		
+		while ($shift > 0)
+		{
+			if ($shift < 60):
+				$diff = $shift;
+				$shift = 0;
+				$term = "second";
+			elseif ($shift < 3600):
+				$diff = floor($shift / 60);
+				$shift %= 60;
+				$term = "minute";
+			elseif ($shift < 86400):
+				$diff = floor($shift / 60 / 60);
+				$shift %= 3600;
+				$term = "hour";
+			else:
+				$diff = floor($shift / 60 / 60 / 24);
+				$shift %= 86400;
+				$term = "day";
+			endif;
+
+			if ($diff != 1) $term .= "s";
+			$ret .= "$diff $term,&nbsp;&nbsp;";
+		}
+		
+		return substr($ret, 0, strlen($ret)-13);
+	}
+?>
 <table border="0">
+	<tr><td><small><b>inverval:</b></small></td><td><small><?=RelativeTime($dbitem->date)?></small></td></tr>
 	<tr><td><small><b>last update:</b></small></td><td><small>
 		<script type="text/javascript">showtime(<?=$dbitem->date."000"?>)</script>
 	</small></td></tr>
