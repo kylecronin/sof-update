07188ca5c4a46cc930df1eb600b3f3d51080a7fe
Kyle Cronin <kyle.cronin@gmail.com>
2009-01-27 15:15:32 -0500

    forgot to git add! oops

---
+++ system/application/views/reputation.php
@@ -0,0 +1,40 @@
+<tr><td colspan="5"><br><h3>reputation <a href="http://stackoverflow.com/questions/61553/track-your-reputation#485031"><font color="#cc2222" size="2">what is this?</font></a></h3></td></tr>
+<?php
+
+//print_r($answers);
+
+$skipped = 0;
+$shown   = 0;
+
+//print_r($answers);
+
+usort($posts, "scoresort");
+
+foreach($posts as $post)
+{
+	$lastQ		= $post['newscore']	- $post['oldscore'];
+
+	if (!$lastQ && !$post['new'])
+	{
+		$skipped++;
+		continue;
+	}
+	else
+		$shown++;
+	
+	echo "<tr";
+	if ($post['new']) echo " class=\"new\"";
+	else if ($post['id'] == $post['qid']) echo " class=\"question\"";
+	echo "><td class=\"".numclass($lastQ)."\" align=\"right\">&nbsp;&nbsp;";
+	echo formatnum($lastQ, false);
+	echo "&nbsp;&nbsp;</td><td colspan=\"2\" align=\"right\">&nbsp;&nbsp;".$post['newscore']."&nbsp;&nbsp;</td>";
+	echo "<td><a ";
+	if ($post['new']) echo "class=\"new\" ";
+	echo "href=\"http://stackoverflow.com/questions/".$post['qid'].'#'.$post['id']."/\">".$post['text']."</td></tr>\n";
+	
+}
+
+echo "<tr><td colspan=\"3\" align=\"right\"><i>$skipped&nbsp;&nbsp;</i></td>";
+echo "<td><i>unchanged not shown</i></td></tr>";
+
+?>
