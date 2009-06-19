0917f06dd67b11eb4a37f2ff3f54ed0d5adb4620
Kyle Cronin <kyle.cronin@gmail.com>
2009-06-19 15:34:33 -0400

    added link to meta-discussion

--- system/application/views/timer.php
+++ system/application/views/timer.php
@@ -55,5 +55,5 @@
 </table>
 <br>
 <a href="http://<?=$site?>/"><font color="444444"><small>back to <?=$site?></small></font></a> &#x2022;
-<a href="http://stackoverflow.com/questions/61553/track-your-reputation"><font color="444444"><small>feedback welcome</small></font></a>
-<!--  &#x2022; <a href="http://stackoverflow.com/questions/61553/track-your-reputation/548370#548370"><font color="cc2222"><small>try it on PEAK</small></font></a> -->
+<a href="http://stackoverflow.com/questions/61553/track-your-reputation"><font color="444444"><small>feedback welcome</small></font></a><br/>
+<font color="red">NEW!</font> <a href="http://sof.modos.org/forum/index.php">(unofficial) StackOverflow meta-discussion board</a>
