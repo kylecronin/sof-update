73e07db8df8866784f49dc1ed5b913aaf434611e
Kyle Cronin <kyle.cronin@gmail.com>
2009-09-28 20:21:05 -0400

    fixed link on error page, added note about current situation

--- system/application/views/site_down.php
+++ system/application/views/site_down.php
@@ -1,13 +1,14 @@
 <html>
 <head><title><?=$sitename?> Reputation Tracker</title></head>
 <body>
+    <h2>NOTE: The site is currently down because the IP has been blocked. I'm working to get it restored. Thanks to everyone that let me know about this issue</h2>
     <h3>An error has occurred accessing <?=$sitename?></h3>
     <p>Please try the following:</p>
     <ol>
         <li>refresh this page</li>
         <li>confirm that <?=$sitename?> is still online</li>
         <li>verify that your user ID number is correct. note: StackOverflow, ServerFault, SuperUser, and Meta do not share the same user ids - you will have a unique one for each site.</li>
-        <li>let me know about this error by <a href="http://stackoverflow.com/questions/61553/track-your-reputation">posting a comment</a></li>
+        <li>let me know about this error by <a href="http://meta.stackoverflow.com/questions/9863/track-your-reputation">posting a comment</a></li>
     </ol>
 </body>
 </html>
